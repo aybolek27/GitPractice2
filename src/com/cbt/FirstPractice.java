5ec8230554f6f95fddf2fc653c2d5596e291ad60
aybolek27 <aybolek27@gmail.com>
2018-09-30 13:04:12 -0500

    added second java file to project

---
+++ src/com/cbt/FirstPractice.java
@@ -0,0 +1,15 @@
+package com.cbt;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class FirstPractice {
+
+	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Aybolek\\Documents\\selenium dependencies\\drivers\\chromedriver.exe");
+
+		WebDriver driver=new ChromeDriver();
+		driver.get("https://google.com");
+	}
+
+}
